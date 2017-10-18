51bb8a3aefbd33806b5ff16199923360f0b80493
Jessica Sena <jessisena@gmail.com>
2017-10-18 15:22:25 +0200

    add json values

--- src/com/esri/cordova/geolocation/utils/JSONHelper.java
+++ src/com/esri/cordova/geolocation/utils/JSONHelper.java
@@ -524,6 +524,29 @@ public final class JSONHelper {
 
             if(nmea.startsWith("$GPGGA")){
                 json.put("nmeaid", "gga");
+
+                SentenceFactory sf = SentenceFactory.getInstance();
+                GGASentence gga = (GGASentence) sf.createParser(nmea);
+
+                try {
+                    json.put("gps_fq", gga.getFixQuality().toString());
+                }catch(DataNotAvailableException e){
+                    Log.d(TAG, "GPS fix qua not available");
+                }
+
+                try {
+                    json.put("hdop", gga.getHorizontalDOP());
+                }catch(DataNotAvailableException e){
+                    Log.d(TAG, "HDOP not available");
+                }   
+                
+                try {
+                    json.put("nmea_lat", gga.getPosition().getLatitude());
+                    json.put("nmea_lon", gga.getPosition().getLongitude());
+                }catch(DataNotAvailableException e){
+                    Log.d(TAG, "Position not available");
+                }                 
+
             }else{
                 json.put("nmeaid", "other");
             }
