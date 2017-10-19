5913ed8978a7d514d3f1156d0e03e9eb86ec3bc9
Jessica Sena <jessisena@gmail.com>
2017-10-19 10:28:58 +0200

    test

--- src/com/esri/cordova/geolocation/utils/JSONHelper.java
+++ src/com/esri/cordova/geolocation/utils/JSONHelper.java
@@ -526,7 +526,7 @@ public final class JSONHelper {
                 json.put("nmeaid", "gga");
 
                 SentenceFactory sf = SentenceFactory.getInstance();
-                GGASentence gga = (GGASentence) sf.createParser(nmea);
+                GGASentence gga = (GGASentence) sf.createParser("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
 
                 /*
                 try {
