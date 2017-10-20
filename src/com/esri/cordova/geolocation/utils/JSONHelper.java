5fdb11b21d955f86b0ecc52a57e21eabb97523b3
Jessica Sena <jessisena@gmail.com>
2017-10-20 10:52:55 +0200

    afegim error gps fq

--- src/com/esri/cordova/geolocation/utils/JSONHelper.java
+++ src/com/esri/cordova/geolocation/utils/JSONHelper.java
@@ -528,14 +528,15 @@ public final class JSONHelper {
                 try{
 
                     SentenceFactory sf = SentenceFactory.getInstance();
-                    GGASentence gga = (GGASentence) sf.createParser("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+                    //GGASentence gga = (GGASentence) sf.createParser("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+                    GGASentence gga = (GGASentence) sf.createParser(nmea);
     
                     json.put("nmeavalid", "VALID");
 
                     try {
                         json.put("gpsfq", gga.getFixQuality().toString());
                     }catch(DataNotAvailableException e){
-                        json.put("gpsfq", "GPS fix qua not available");
+                        json.put("gpsfq", "0");
                     }
     
                     
